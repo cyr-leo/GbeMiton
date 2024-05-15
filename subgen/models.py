04e484ba4e16d155d18f4b2b4cbae77d15125269
Léo <134875150+cyr-leo@users.noreply.github.com>
2024-05-15 15:57:02 +0100

    change video

--- subgen/models.py
+++ subgen/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class vid(models.Model):
 
     url = models.FileField(upload_to='videos/', null=True)
-    urll = models.URLField(null=True)
     description = models.TextField(null=True, blank=True)
     name = models.CharField(max_length=120, null=True)
     cap_fil = models.FileField(upload_to='subtitles/', null=True)
